3150db0900f100c658da13f4c88fbb32d2d93931
Michael Hammerl <michael.hammerl1996@gmail.com>
2016-07-29 16:36:17 +0200

    defined some basic newslist-GUI

--- resources/views/shownewslistfe.blade.php
+++ resources/views/shownewslistfe.blade.php
@@ -1,4 +1,38 @@
 @extends('masterlayoutfiles.frontendmaster')
 @section('rightcol_content_lvl2')
-    @parent
+    <div class="jumbotron">
+        <h2>Pläne für Trainingslager stehen <span class="label label-default">New</span></h2>
+        <p class="jumbop">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor... </p>
+        <p>
+            <span class="label label-default">Default</span>
+            <span class="label label-primary">Primary</span>
+            <span class="label label-success">Success</span>
+            <span class="label label-info">Info</span>
+            <span class="label label-warning">Warning</span>
+            <span class="label label-danger">Danger</span>
+        </p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3>Saisonstart festgelegt! <span class="label label-default">New</span></h3>
+        </div>
+        <div class="panel-body">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </div>
+        <div class="panel-footer">
+            <span class="label label-default">Default</span>
+            <span class="label label-primary">Primary</span>
+            <span class="label label-success">Success</span>
+            <span class="label label-info">Info</span>
+            <span class="label label-warning">Warning</span>
+            <span class="label label-danger">Danger</span>
+        </div>
+        <div class="panel-footer">
+            Erstellt: 19.07.2016 von <a href="#">hammerlm</a>
+        </div>
+        <div class="panel-footer">
+            Zuletzt modifiziert: 19.07.2016 von <a href="#">hammerlm</a>
+        </div>
+    </div>
 @stop
